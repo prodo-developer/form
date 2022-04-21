97d5e38e44fb5bb430309fe67b1185a916c7df9a
prodo-developer <wjdals300@gmail.com>
2022-04-21 23:45:57 +0900

    feat: 체크박스를 이용한 EnumType, 공통 코드 추가

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,13 +2,20 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Item {
 
-    private Long id;
+    private long id;
     private String itemName;
-    private Integer price;
-    private Integer quantity;
+    private int price;
+    private int quantity;
+
+    private boolean open; //판매여부
+    private List<String> regions; // 등록지역
+    private ItemType itemType; // 상품 종류
+    private String deliveryCode; // 배송 방식
 
     public Item() {
     }
